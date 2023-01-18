046eb27b453300ba6e1b4cbb381e83c5b7729cab
tejasaneraoBCT <Tejas.Anerao@blueconchtech.com>
2023-01-18 15:48:54 +0530

    Added UnitTesting Project

---
+++ 7. UnitTesting/Misc/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    public class Calculator
+    {
+        public double AddNumbers(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
