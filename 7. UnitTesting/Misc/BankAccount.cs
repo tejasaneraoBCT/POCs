47208a3be1437ecea0e6744fe4e246ae59492bc6
tejasaneraoBCT <Tejas.Anerao@blueconchtech.com>
2023-01-24 12:23:16 +0530

    Added BankAccount and Logger

---
+++ 7. UnitTesting/Misc/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+        private readonly ILogBook _logBook;
+
+        public BankAccount(ILogBook logBook)
+        {
+            Balance = 0;
+            _logBook = logBook;
+        }
+
+        public bool Deposit(int amount)
+        {
+            Balance += amount;
+            _logBook.Log($"INR {amount} where credited to your account");
+            return true;
+        }
+
+        public bool Withdrawal(int amount)
+        {
+            if ((Balance - amount) >= 0)
+            {
+                Balance -= amount;
+                _logBook.Log($"INR {amount} where debited from your account");
+                return true;
+            }
+
+            _logBook.Log($"Debit transaction of INR {amount} failed due to insufficient balance");
+            return false;
+        }
+
+        public int GetBalance() => Balance;
+
+    }
+}
---
+++ 7. UnitTesting/Misc/LogBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    public interface ILogBook
+    {
+        void Log(string message);
+    }
+
+    public class LogBook : ILogBook
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
